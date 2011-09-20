af0ba969a585eb9d30fb84cf043537cae2ce2693
Ken Egozi <mail@kenegozi.com>
2011-09-20 17:24:27 +0300

    add socket.io magic!

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express'), path = require('path');
+var socketio = require('socket.io');
+
 
 var app = module.exports = express.createServer().configure(function() {
     this.set('views', path.join(__dirname, "views"));
@@ -15,5 +17,16 @@ var cellData =  {
 app.get('/grid', function (req, res) {
     res.render('grid', {cellData: cellData});
 });
+
+var io = socketio.listen(app);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('cellEdit', function(data) {
+		console.log('got message: ' + JSON.stringify(data));
+		cellData[data.cellName] = data.val;
+		socket.broadcast.emit('cellEdit', data);
+	});
+});
+
 
 app.listen(8080);
